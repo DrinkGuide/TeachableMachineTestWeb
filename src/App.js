e5495542dfdc5dafd7316221f1b7286825525e47
0213sua <0213tndk@gmail.com>
2024-07-21 21:04:23 +0900

    구글 로그인 요청

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 // src/App.js
 import React from "react";
-import TeachableMachine from "./TeachableMachine";
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import TeachableMachine from './components/TeachableMachine';
 
 function App() {
     return (
-        <div className="App">
-            <TeachableMachine />
-        </div>
+        <Router>
+            <div className="App">
+            <header className="App-header">
+             <h1>My App</h1>
+             </header>
+             <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/teachable-machine" element={<TeachableMachine />} />
+        </Routes>
+      </div>
+    </Router>
     );
 }
 
